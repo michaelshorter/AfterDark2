52bb5d281d72996fedc2779192106c0bbd005a9d
michaelshorter <mail@mrshorter.co.uk>
2026-05-20 10:54:10 +0100

    Mirror displays, token flow, cursor fix, correct paths

--- run_jukebox.sh
+++ run_jukebox.sh
@@ -3,11 +3,12 @@ export DISPLAY=:0
 export XAUTHORITY=/home/jukebox/.Xauthority
 
 # Hide the cursor at OS level so VLC can't bring it back
+sleep 2
 unclutter -idle 0 -root &
 
-# Set up display output - wait for displays to initialise first
-sleep 10
-xrandr --output HDMI-2 --auto --primary
+# Set up mirrored displays
+sleep 5
+xrandr --output HDMI-A-1 --auto --output HDMI-A-2 --auto --same-as HDMI-A-1
 
 # Wait for USB drive to mount
 echo "Waiting for USB drive..."
@@ -17,4 +18,4 @@ done
 echo "USB drive found, starting jukebox..."
 
 source /home/jukebox/jukebox-env/bin/activate
-python /home/jukebox/AfterDark2/jukebox.py
+python /home/jukebox//AfterDark2/jukebox.py
